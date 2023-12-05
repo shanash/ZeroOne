fd0560233093c5cbd16bc4152a4d788d5b74918c
안성훈 <seonghun@fluffyduck.co.kr>
2023-12-05 16:11:56 +0900

    유니티 메뉴에서 엑셀 데이터를 json 및 cs로 변경할 수 있도록 수정

---
+++ Assets/Scripts/Editor/ConvertExcel.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using System.Diagnostics;
+
+public class ConvertExcel
+{
+    [MenuItem("FluffyDuck/Run Covert Excel To Json")]
+    private static void RunConsoleProgram()
+    {
+        string fullpath = "Android\\Excel2Json\\Excel2Json.exe"; // 폴더 경로
+        string arguments = "-d Android\\ExcelData -o Assets\\AssetResources\\Master -cs Assets\\Scripts\\MasterData"; // 전달할 파라미터들
+
+        Process process = new Process();
+        process.StartInfo.FileName = fullpath;
+        process.StartInfo.Arguments = arguments; // 파라미터 전달
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.StandardOutputEncoding = System.Text.Encoding.GetEncoding("EUC-KR");
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        UnityEngine.Debug.Log(output); // 유니티 콘솔에 출력
+    }
+}
