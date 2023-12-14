081ec08c2c0d862f260fd2706377aac4359535ec
안성훈 <seonghun@fluffyduck.co.kr>
2023-12-14 09:32:08 +0900

    코루틴 매니저 쓸데없는코드 삭제

--- Assets/Scripts/Common/CoroutineManager.cs
+++ Assets/Scripts/Common/CoroutineManager.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using UnityEngine;
 
+/// <summary>
+/// 일반 클래스에서도 코루틴을 사용할 수 있게 도와주는 역할...인데
+/// 딱히 관리를 빡세게 할 필요를 아직 못 느끼겠다
+/// </summary>
 public class CoroutineManager : MonoBehaviourSingleton<CoroutineManager>
 {
-    protected override bool _Is_DontDestroyOnLoad { get { return true; } }
-
-    public Coroutine StartManagedCoroutine(IEnumerator coroutine)
-    {
-        return StartCoroutine(coroutine);
-    }
+    protected override bool _Is_DontDestroyOnLoad => true;
 }
