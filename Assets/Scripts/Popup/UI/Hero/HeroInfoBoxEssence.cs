607c12aaf080d661732f3e06be91d89d6be10ac7
안성훈 <seonghun@fluffyduck.co.kr>
2024-01-22 12:49:18 +0900

    ㅇㅇㅇ

---
+++ Assets/Scripts/Popup/UI/Hero/HeroInfoBoxEssence.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+
+public class HeroInfoBoxEssence : MonoBehaviour
+{
+    //근원 전달 달성률
+    [SerializeField]
+    TMP_Text TransferChange_Subject;
+
+    [SerializeField]
+    TMP_Text TransferChangePercent_Value;
+
+    //전투력
+    [SerializeField]
+    TMP_Text CombatPower_Subject;
+
+    [SerializeField]
+    TMP_Text AddedCombatPower_Value;
+
+    //전달반응
+    [SerializeField]
+    TMP_Text TransferReaction_Subject;
+
+    [SerializeField]
+    TMP_Text
+
+    public void OnClickBuffDetailButton()
+    {
+
+    }
+
+    public void OnClickTransferReactionInfoButton()
+    {
+
+    }
+
+    public void OnClickTransferEssenceButton()
+    {
+
+    }
+}
