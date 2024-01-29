ae9baf3968867443ce2220defafc07440777a55d
안성훈 <seonghun@fluffyduck.co.kr>
2024-01-29 10:26:20 +0900

    폴더 이동

--- Assets/Scripts/Common/Define/Essence_Status_Data_Define.cs
+++ Assets/Scripts/Common/Define/Essence_Status_Data_Define.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public static class Essence_Status_Data_Define
 {
     public static string ToStringForUI(this Essence_Status_Data value)
