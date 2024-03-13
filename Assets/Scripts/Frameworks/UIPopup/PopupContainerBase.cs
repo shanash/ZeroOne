fd36340f47ef2b6abe8b954e0911e30714f52007
안성훈 <seonghun@fluffyduck.co.kr>
2024-03-13 10:18:57 +0900

    작업 수정

--- Assets/Scripts/Frameworks/UIPopup/PopupContainerBase.cs
+++ Assets/Scripts/Frameworks/UIPopup/PopupContainerBase.cs
@@ -1,14 +1,15 @@
 using FluffyDuck.Util;
 using System.Collections.Generic;
 using UnityEngine;
-using FluffyDuck.UI;
 
-public abstract class PopupContainerBase : MonoBehaviour, IPoolableComponent
+namespace FluffyDuck.UI
 {
-    public abstract Dictionary<POPUP_TYPE, RectTransform> Get { get; }
+    public abstract class PopupContainerBase : MonoBehaviour, IPoolableComponent
+    {
+        public abstract Dictionary<POPUP_TYPE, RectTransform> Get { get; }
 
-    public abstract void SetUICamera();
-
-    public abstract void Despawned();
-    public abstract void Spawned();
+        public abstract void SetUICamera();
+        public abstract void Despawned();
+        public abstract void Spawned();
+    }
 }
